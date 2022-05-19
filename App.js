203d35360089bfe158e5a4a7e5fccb39c3c246d3
wajisan <wadi.kihel@epitech.eu>
2022-05-19 03:22:27 +0200

    new design + almost working refresh (need to have a better update)

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { StyleSheet, Text, View, Keyboard } from 'react-native';
 import AddAccount  from './components/AddAccount';
 //import Account from './components/Account';
 import BlockChain from './components/BlockChain';
+import Icon from './components/Icon';
 //<Account stakeAddress="stake1u8a7w862g9g35vgpnzfvk299zlvjy26xp2w8c3dkjlfgzyc3vgu24"></Account>
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Waji Pool :</Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.sectionTitle}><Icon/></Text>
         <BlockChain></BlockChain>
         <AddAccount></AddAccount>
       </View>
@@ -18,16 +19,17 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
- /*container: {
+ container: {
     flex: 1,
     backgroundColor: '#E8EAED',
   },
-  tasksWrapper: {
-    paddingTop: 80,
+  wrapper: {
+    paddingTop: 15,
     paddingHorizontal: 20,
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold'
-  }*/
+    fontWeight: 'bold',
+    display: 'flex'
+  }
 });
